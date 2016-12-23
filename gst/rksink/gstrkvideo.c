e465df98c7304bad6d466a1ceef679500384da3c
Randy Li <randy.li@rock-chips.com>
2016-12-23 14:55:39 +0800

    rkvideo: move all the video sink to this plugin package
    The all the plugins in this package now support dmabuf.
    
    Signed-off-by: Randy Li <randy.li@rock-chips.com>

--- gst/rksink/gstrkvideo.c
+++ gst/rksink/gstrkvideo.c
@@ -58,6 +58,6 @@ plugin_init (GstPlugin * plugin)
 
 GST_PLUGIN_DEFINE (GST_VERSION_MAJOR,
     GST_VERSION_MINOR,
-    rksink,
+    rkvideo,
     "Rockchip Video Sink",
     plugin_init, VERSION, GST_LICENSE, GST_PACKAGE_NAME, GST_PACKAGE_ORIGIN);
