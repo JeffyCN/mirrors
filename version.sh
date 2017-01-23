7fb51afbadde758fb01786c4e347afb0f84003a5
Zikim,Wei <wzq@rock-chips.com>
2017-01-23 10:53:19 +0800

    Remove build time from rga library
    Change-Id: I843542fa6794dd2e8915e3bcee6b9130e43ff935
    Signed-off-by: Zikim,Wei <wzq@rock-chips.com>

--- version.sh
+++ version.sh
@@ -3,8 +3,9 @@ rm -f version.h
 
 COMMIT_ID=$(cd $(dirname $0) && git log -1 | awk 'NR==1{print}')
 SUB_COMMIT_ID=$(echo "$COMMIT_ID" | cut -c8-14)
-CUR_TIME=$(date "+%G-%m-%d %H:%M:%S")
-RK_GRAPHICS_VERSION=$SUB_COMMIT_ID+"$CUR_TIME"
+#CUR_TIME=$(date "+%G-%m-%d %H:%M:%S")
+#RK_GRAPHICS_VERSION=$SUB_COMMIT_ID+"$CUR_TIME"
+RK_GRAPHICS_VERSION=$SUB_COMMIT_ID
 
 cd $(dirname $0) && cat version.h.template | sed "s/\$FULL_VERSION/$RK_GRAPHICS_VERSION/g" > version.h
 echo "Generated version.h"
