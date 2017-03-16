76255324387fe904d4949d9b4ce63dffef49c69c
Zikim,Wei <wzq@rock-chips.com>
2017-03-16 11:04:22 +0800

    Fix input/output file name from yuv to bin
    Change-Id: Id2b286c478e9cddb618d35984d1bd239c78dbd77
    Signed-off-by: Zikim,Wei <wzq@rock-chips.com>

--- tests/RockchipFileOps.cpp
+++ tests/RockchipFileOps.cpp
@@ -143,7 +143,7 @@ int get_string_by_format(char *value, int format)
 
 int get_buf_from_file(void *buf, int f, int sw, int sh, int index)
 {
-    const char *inputFilePath = "/data/in%dw%d-h%d-%s.yuv";
+    const char *inputFilePath = "/data/in%dw%d-h%d-%s.bin";
     char filePath[100];
     char fstring[30];
     int ret = 0;
@@ -180,7 +180,7 @@ int get_buf_from_file(void *buf, int f, int sw, int sh, int index)
 
 int output_buf_data_to_file(void *buf, int f, int sw, int sh, int index)
 {
-    const char *outputFilePath = "/data/out%dw%d-h%d-%s.yuv";
+    const char *outputFilePath = "/data/out%dw%d-h%d-%s.bin";
     char filePath[100];
     char fstring[30];
     int ret = 0;
