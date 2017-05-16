509e576c2c237341f5cbd05bce34deb3fec21e52
Randy Li <randy.li@rock-chips.com>
2017-05-16 11:05:41 +0800

    build: update some searching rules with upstream
    The Gstreamer upstream has some update for configure rules,
    especially in searching packages and apply its pkgconfig.
    
    The introduction of the mesonbuild is in plan.
    
    Signed-off-by: Randy Li <randy.li@rock-chips.com>
    Signed-off-by: ayaka <ayaka@soulik.info>

--- gst/rockchipmpp/gstmpp.c
+++ gst/rockchipmpp/gstmpp.c
@@ -41,6 +41,6 @@ plugin_init (GstPlugin * plugin)
 
 GST_PLUGIN_DEFINE (GST_VERSION_MAJOR,
     GST_VERSION_MINOR,
-    mppvideo,
+    rockchipmpp,
     "Rockchip Mpp Video Plugin",
     plugin_init, VERSION, GST_LICENSE, GST_PACKAGE_NAME, GST_PACKAGE_ORIGIN);
