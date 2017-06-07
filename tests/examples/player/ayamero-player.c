6d5e719b45c1905e1c2262a93986b516c77a562e
ayaka <ayaka@soulik.info>
2017-06-07 22:31:30 +0800

    tests: examples: add more features to ayamero
    Somebody wants them.
    1. allow setting the video and audio sink in player
    2. forward and backward
    
    Signed-off-by: ayaka <ayaka@soulik.info>

--- tests/examples/player/ayamero-player.c
+++ tests/examples/player/ayamero-player.c
@@ -28,6 +28,8 @@ typedef struct _PlayerData
   GstElement *pipeline;
   GMainLoop *main_loop;
   GList *playlist;
+  GstElement *vsink;
+  GstElement *asink;
 } PlayerData;
 
 static gboolean
@@ -77,12 +79,37 @@ handle_message (GstBus * bus, GstMessage * msg, PlayerData * data)
           &pending_state);
     }
       break;
+    case GST_MESSAGE_ASYNC_DONE:{
+      g_print ("async done.\n");
+    }
+      break;
     default:
       break;
   }
 
   /* We want to keep receiving messages */
   return TRUE;
+}
+
+static gboolean
+seek_to_offsets (GstElement * pipeline, gint64 seconds)
+{
+  gint64 pos;
+  GstState state;
+
+  if (GST_STATE_CHANGE_SUCCESS != gst_element_get_state (pipeline, &state, NULL,
+          GST_SECOND))
+    return FALSE;
+  if (state != GST_STATE_PLAYING && state != GST_STATE_PAUSED)
+    return FALSE;
+
+  if (gst_element_query_position (pipeline, GST_FORMAT_TIME, &pos)) {
+    return gst_element_seek_simple (pipeline, GST_FORMAT_TIME,
+        GST_SEEK_FLAG_FLUSH | GST_SEEK_FLAG_KEY_UNIT,
+        pos + seconds * GST_SECOND);
+  }
+
+  return FALSE;
 }
 
 static gboolean
@@ -109,13 +136,23 @@ keypress (GIOChannel * source, GIOCondition condition, PlayerData * data)
       }
         break;
       case 'p':{
-        GstState cur;
-        cur = GST_STATE (data->pipeline);
-        if (cur == GST_STATE_PLAYING)
+        GstState state;
+
+        if (GST_STATE_CHANGE_SUCCESS != gst_element_get_state
+            (data->pipeline, &state, NULL, GST_SECOND))
+          break;
+
+        if (state == GST_STATE_PLAYING)
           gst_element_set_state (data->pipeline, GST_STATE_PAUSED);
-        if (cur == GST_STATE_PAUSED)
+        if (state == GST_STATE_PAUSED)
           gst_element_set_state (data->pipeline, GST_STATE_PLAYING);
       }
+        break;
+      case 'f':
+        seek_to_offsets (data->pipeline, 30L);
+        break;
+      case 'b':
+        seek_to_offsets (data->pipeline, -10L);
         break;
       default:
         break;
@@ -175,12 +212,18 @@ main (gint argc, gchar * argv[])
   GstBus *bus = NULL;
   GError *err = NULL;
 
-  gchar *uri = NULL, *path = NULL;
+  gchar *uri = NULL, *path = NULL, *vsink = NULL, *asink = NULL;
   GOptionEntry options[] = {
     {"uri", '\0', 0, G_OPTION_ARG_STRING, &uri, "video source URI", NULL}
     ,
     {"path", '\0', 0, G_OPTION_ARG_STRING, &path,
         "the path of a video file or directory", NULL}
+    ,
+    {"video-sink", '\0', 0, G_OPTION_ARG_STRING, &vsink,
+        "the video sink", NULL}
+    ,
+    {"audio-sink", '\0', 0, G_OPTION_ARG_STRING, &asink,
+        "the audio sink", NULL}
     ,
     {NULL}
   };
@@ -213,9 +256,16 @@ main (gint argc, gchar * argv[])
     uri = data.playlist->data;
   }
 
+  if (vsink != NULL)
+    data.vsink = gst_element_factory_make (vsink, "video-sink");
+  if (asink != NULL)
+    data.asink = gst_element_factory_make (asink, "audio-sink");
+
   data.main_loop = g_main_loop_new (NULL, FALSE);
   data.pipeline = gst_element_factory_make ("playbin", "play");
   g_object_set (G_OBJECT (data.pipeline), "uri", uri, NULL);
+  g_object_set (G_OBJECT (data.pipeline), "video-sink", data.vsink, NULL);
+  g_object_set (G_OBJECT (data.pipeline), "audio-sink", data.asink, NULL);
 
   io_stdin = g_io_channel_unix_new (fileno (stdin));
   g_io_add_watch (io_stdin, G_IO_IN, (GIOFunc) keypress, (gpointer) & data);
