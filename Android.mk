4e0d5d71bde2421f0de38b7e990582ddcf44ceb7
Tim Zhou <tim.zhou@rock-chips.com>
2017-09-21 17:55:06 +0800

    Move librga to vendor partition
    Change-Id: I68dfb7da5e09de92e6b612fdc173373dbd6e3a08

--- Android.mk
+++ Android.mk
@@ -69,6 +69,7 @@ LOCAL_CFLAGS += -DRK3368
 endif
 
 LOCAL_MODULE:= librga
+LOCAL_PROPRIETARY_MODULE := true
 include $(BUILD_SHARED_LIBRARY)
 endif
 #############################################################################################
@@ -85,7 +86,7 @@ LOCAL_SRC_FILES += \
         normal/NormalRgaApi.cpp
 
 LOCAL_MODULE := librga
-
+LOCAL_PROPRIETARY_MODULE := true
 LOCAL_C_INCLUDES += external/libdrm/rockchip
 LOCAL_C_INCLUDES += hardware/rockchip/libgralloc
 LOCAL_C_INCLUDES += hardware/rk29/libgralloc_ump
