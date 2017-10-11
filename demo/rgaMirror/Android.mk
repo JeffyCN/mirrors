3a82503e7c2064410d1d0a54b3a7aee44fdedd5a
putinlee <putin.li@rock-chips.com>
2017-10-11 11:12:49 +0800

    Provided multiple demo,they can build in specified folder.
    Change-Id: If8d1a6ca085df0049e1f123a174754965dad92ce
    Signed-off-by: putinlee <putin.li@rock-chips.com>

---
+++ demo/rgaMirror/Android.mk
@@ -0,0 +1,71 @@
+LOCAL_PATH:= $(call my-dir)
+#======================================================================
+#
+#rgaMirror
+#
+#======================================================================
+include $(CLEAR_VARS)
+
+LOCAL_CFLAGS += -DGL_GLEXT_PROTOTYPES -DEGL_EGLEXT_PROTOTYPES
+
+LOCAL_CFLAGS += -DROCKCHIP_GPU_LIB_ENABLE
+
+LOCAL_CFLAGS += -Wall -Werror -Wunreachable-code
+
+LOCAL_C_INCLUDES += external/tinyalsa/include
+
+LOCAL_C_INCLUDES += hardware/rockchip/librga
+LOCAL_C_INCLUDES += hardware/rk29/librga
+
+LOCAL_SHARED_LIBRARIES := \
+    libcutils \
+    liblog \
+    libutils \
+    libbinder \
+    libui \
+    libEGL \
+    libGLESv1_CM \
+    libgui \
+    libhardware \
+    librga
+
+#has no "external/stlport" from Android 6.0 on
+ifeq (1,$(strip $(shell expr $(PLATFORM_VERSION) \< 6.0)))
+LOCAL_C_INCLUDES += \
+    external/stlport/stlport
+
+LOCAL_SHARED_LIBRARIES += \
+    libstlport
+
+LOCAL_C_INCLUDES += bionic
+endif
+
+ifeq ($(strip $(BOARD_USE_DRM)), true)
+ifneq (1,$(strip $(shell expr $(PLATFORM_VERSION) \< 6.9)))
+LOCAL_CFLAGS += -DANDROID_7_DRM
+endif
+endif
+
+ifeq ($(strip $(TARGET_BOARD_PLATFORM)),rk3368)
+LOCAL_CFLAGS += -DRK3368
+ifneq (1,$(strip $(shell expr $(PLATFORM_VERSION) \< 8.0)))
+LOCAL_CFLAGS += -DRK3368_ANDROID_8
+endif
+endif
+
+ifeq ($(strip $(TARGET_BOARD_PLATFORM)),rk3188)
+LOCAL_CFLAGS += -DRK3188
+endif
+
+LOCAL_SRC_FILES:= \
+	RockchipFileOps.cpp \
+    rgaMirror.cpp
+
+LOCAL_MODULE:= rgaMirror
+
+ifdef TARGET_32_BIT_SURFACEFLINGER
+LOCAL_32_BIT_ONLY := true
+endif
+
+include $(BUILD_EXECUTABLE)
+
