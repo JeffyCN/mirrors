c92b39b0baf59e8ac2efd6b99f76b6948d232295
ZhongYichong <zyc@rock-chips.com>
2018-08-06 14:27:27 +0800

    add runtime log level switch to camera engine
    android:
      setprop persist.camera.engine.log <level>
    linux:
      export persist_camera_engine_log=<level>
    
    level could be the followings:
      0: error level, defalut level
      1: warniing level
      2: info level
      3: verbose level
      4: debug level
    
    Change-Id: I0e8011f14780e7606fa3a44a489dc4ee0dfcc15c
    Signed-off-by: ZhongYichong <zyc@rock-chips.com>

--- xcore/xcam_common.cpp
+++ xcore/xcam_common.cpp
@@ -89,6 +89,21 @@ xcam_fourcc_to_string (uint32_t fourcc)
     return str;
 }
 
+bool xcam_get_enviroment_value(const char* variable, int* value)
+{
+    if (!variable || !value) {
+         return false;
+
+    }
+
+    char* valueStr = getenv(variable);
+    if (valueStr) {
+        *value = strtoul(valueStr, nullptr, 0);
+        return true;
+    }
+    return false;
+}
+
 void xcam_print_log (int level, const char* format, ...) {
     char buffer[XCAM_MAX_STR_SIZE] = {0};
 
@@ -97,43 +112,50 @@ void xcam_print_log (int level, const char* format, ...) {
     vsnprintf (buffer, XCAM_MAX_STR_SIZE, format, va_list);
     va_end (va_list);
 
-#ifdef ANDROID_OS
-    switch(level) {
-    case ERROR_LEVEL:
-        ALOGE("%s", buffer);
-        break;
-    case WARNING_LEVEL:
-        ALOGW("%s", buffer);
-        break;
-    case INFO_LEVEL:
-        ALOGI("%s", buffer);
-        break;
-    case VERBOSE_LEVEL:
-        ALOGV("%s", buffer);
-        break;
-    case DEBUG_LEVEL:
-        ALOGD("%s", buffer);
-        break;
-    default:
-        ALOGE("debug level not support");
-        break;
-    }
-#endif
     if (strlen (log_file_name) > 0) {
         FILE* p_file = fopen (log_file_name, "ab+");
         if (NULL != p_file) {
             fwrite (buffer, sizeof (buffer[0]), strlen (buffer), p_file);
             fclose (p_file);
         } else {
-#ifndef ANDROID_OS
-            printf ("%s", buffer);
-#endif
+            printf("error! can't open log file !\n");
         }
-    } else {
-#ifndef ANDROID_OS
+        return ;
+    }
+#ifdef ANDROID_OS
+    char property_value[50] = {0};
+
+    property_get("persist.camera.engine.log", property_value, "0");
+    int cam_engine_log_level = atoi(property_value);
+
+    if (level <= cam_engine_log_level) {
+        switch(level) {
+        case ERROR_LEVEL:
+            ALOGE("%s", buffer);
+            break;
+        case WARNING_LEVEL:
+            ALOGW("%s", buffer);
+            break;
+        case INFO_LEVEL:
+            ALOGI("%s", buffer);
+            break;
+        case VERBOSE_LEVEL:
+            ALOGV("%s", buffer);
+            break;
+        case DEBUG_LEVEL:
+            ALOGD("%s", buffer);
+            break;
+        default:
+            ALOGE("debug level not support");
+            break;
+        }
+    }
+#else
+    int cam_engine_log_level = 0;
+    xcam_get_enviroment_value("persist_camera_engine_log", &cam_engine_log_level);
+    if (level <= cam_engine_log_level)
         printf ("%s", buffer);
 #endif
-    }
 }
 
 void xcam_set_log (const char* file_name) {
