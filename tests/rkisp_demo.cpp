3f3e12b88c079d7f1b2bab9e28fc5e94cb2d0608
ZhongYichong <zyc@rock-chips.com>
2018-11-21 15:01:23 +0800

    avoid using function like gstxxx in rkisp_demo
    Change-Id: Ia42035613b097c3c0d8821589805084127736912
    Signed-off-by: ZhongYichong <zyc@rock-chips.com>

--- tests/rkisp_demo.cpp
+++ tests/rkisp_demo.cpp
@@ -21,7 +21,7 @@
 #include <linux/videodev2.h>
 #include <rkisp_control_loop.h>
 #include <rkisp_dev_manager.h>
-#include <media-controller.h>
+#include "mediactl.h"
 
 #define CLEAR(x) memset(&(x), 0, sizeof(x))
 #define FMT_NUM_PLANES 1
@@ -48,11 +48,11 @@ struct RKIspFunc _RKIspFunc;
 struct RKisp_media_ctl
 {
     /* media controller */
-    GstMediaController *controller;
-    GstMediaEntity *isp_subdev;
-    GstMediaEntity *isp_params_dev;
-    GstMediaEntity *isp_stats_dev;
-    GstMediaEntity *sensor_subdev;
+    media_device *controller;
+    media_entity *isp_subdev;
+    media_entity *isp_params_dev;
+    media_entity *isp_stats_dev;
+    media_entity *sensor_subdev;
 };
 struct buffer {
         void *start;
@@ -159,24 +159,33 @@ static void start_capturing(void)
 		}
     	if (_RKIspFunc.prepare_func != NULL) {
 			struct rkisp_cl_prepare_params_s params={0};
+            int nents;
+
             rkisp.controller =
-                gst_media_controller_new_by_vnode (dev_name);
+                media_device_new (dev_name);
             if (!rkisp.controller)
                 errno_exit(
                     "Can't find controller, maybe use a wrong video-node or wrong permission to media node");
+            /* Enumerate entities, pads and links. */
+            media_device_enumerate (rkisp.controller);
             rkisp.isp_subdev =
-              gst_media_find_entity_by_name (rkisp.controller, "rkisp1-isp-subdev");
+              media_get_entity_by_name (rkisp.controller, "rkisp1-isp-subdev",
+                                        strlen("rkisp1-isp-subdev"));
             rkisp.isp_params_dev =
-              gst_media_find_entity_by_name (rkisp.controller, "rkisp1-input-params");
+              media_get_entity_by_name (rkisp.controller, "rkisp1-input-params",
+                                        strlen("rkisp1-input-params"));
             rkisp.isp_stats_dev =
-              gst_media_find_entity_by_name (rkisp.controller, "rkisp1-statistics");
+              media_get_entity_by_name (rkisp.controller, "rkisp1-statistics",
+                                        strlen("rkisp1-statistics"));
              /* assume the last enity is sensor_subdev */
-            rkisp.sensor_subdev = gst_media_get_last_entity (rkisp.controller);
+            nents = media_get_entities_count (rkisp.controller);
+            rkisp.sensor_subdev = media_get_entity (rkisp.controller, nents - 1);
 
             params.isp_sd_node_path = media_entity_get_devname (rkisp.isp_subdev);
             params.isp_vd_params_path = media_entity_get_devname (rkisp.isp_params_dev);
             params.isp_vd_stats_path = media_entity_get_devname (rkisp.isp_stats_dev);
             params.sensor_sd_node_path = media_entity_get_devname (rkisp.sensor_subdev);
+            media_device_unref (rkisp.controller);
             /*
             // isp subdev node path
             params.isp_sd_node_path="/dev/v4l-subdev0";
