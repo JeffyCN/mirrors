f9c2f791d84a8bf93e6a772e57460ba6118197fc
Zain Wang <wzz@rock-chips.com>
2018-11-28 19:24:41 +0800

    linux_pack_firmware: update rk3328-mkupdate.sh
    Change -RK3328 to -RK322H
    
    Change-Id: I29fdca134d45a51859f5e66304ec46e4f2e9cf9a
    Signed-off-by: Zain Wang <wzz@rock-chips.com>

--- linux/Linux_Pack_Firmware/rockdev/rk3328-mkupdate.sh
+++ linux/Linux_Pack_Firmware/rockdev/rk3328-mkupdate.sh
@@ -15,7 +15,7 @@ if [ ! -f "package-file" ]; then
 	exit 1
 fi
 ./afptool -pack ./ Image/update.img || pause
-./rkImageMaker -RK3328 Image/MiniLoaderAll.bin Image/update.img update.img -os_type:androidos || pause
+./rkImageMaker -RK322H Image/MiniLoaderAll.bin Image/update.img update.img -os_type:androidos || pause
 echo "Making update.img OK."
 #echo "Press any key to quit:"
 #read -n1 -s key
