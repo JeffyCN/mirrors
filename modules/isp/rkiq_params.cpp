e251a7c4a639b7a7617870223a70b72e39c6a350
ZhongYichong <zyc@rock-chips.com>
2018-12-21 14:28:31 +0800

    fix dpcc check error for ISP V12
    Change-Id: I49a2fa3a155c380412447e4e44d1feb4ddebb41f
    Signed-off-by: ZhongYichong <zyc@rock-chips.com>

--- modules/isp/rkiq_params.cpp
+++ modules/isp/rkiq_params.cpp
@@ -36,11 +36,19 @@
 struct AiqResults mLastAiqResults = {0};
 struct rkisp_parameters mLastRkiqResults = {0};
 
-static int dpcc_param_check(struct cifisp_dpcc_config* arg)
+static int dpcc_param_check(struct cifisp_dpcc_config* arg, int isp_ver)
 {
     unsigned int i = 0;
     struct cifisp_dpcc_methods_config* method;
-    if (arg->mode > CIF_ISP_DPCC_MODE_MAX || arg->output_mode > CIF_ISP_DPCC_OUTPUTMODE_MAX || arg->set_use > CIF_ISP_DPCC_SETUSE_MAX) {
+    // ISP v12 dpcc uses some more bits which are not used in isp v10, and
+    // ISP V12 supports two dpcc modes, including a new one, and the old one
+    // which is the same as V10. Because the dpcc params defined in tuning xml
+    // use the V12's format for the reason compatible with V10, so we just check
+    // the parpams with V12 here.
+
+    if (arg->mode > CIF_ISP_DPCC_MODE_MAX ||
+        arg->output_mode > CIF_ISP_DPCC_OUTPUTMODE_MAX_V12 ||
+        arg->set_use > CIF_ISP_DPCC_SETUSE_MAX) {
         LOGE("%s:%d check error !", __FUNCTION__, __LINE__);
         goto err;
     }
@@ -50,16 +58,21 @@ static int dpcc_param_check(struct cifisp_dpcc_config* arg)
     }
     method = &arg->methods[i];
     for (i = 0; i < CIFISP_DPCC_METHODS_MAX; i++) {
-        if ((method->method & CIF_ISP_DPCC_METHODS_SET_RESERVED) || (method->line_thresh & CIF_ISP_DPCC_LINE_THRESH_RESERVED) || (method->line_mad_fac & CIF_ISP_DPCC_LINE_MAD_FAC_RESERVED)) {
+        if ((method->method & CIF_ISP_DPCC_METHODS_SET_RESERVED_V12) ||
+            (method->line_thresh & CIF_ISP_DPCC_LINE_THRESH_RESERVED_V12) ||
+            (method->line_mad_fac & CIF_ISP_DPCC_LINE_MAD_FAC_RESERVED)) {
             LOGE("%s:%d check error !", __FUNCTION__, __LINE__);
             goto err;
         }
-        if ((method->pg_fac & CIF_ISP_DPCC_PG_FAC_RESERVED) || (method->rnd_thresh & CIF_ISP_DPCC_RND_THRESH_RESERVED) || (method->rg_fac & CIF_ISP_DPCC_RG_FAC_RESERVED)) {
+        if ((method->pg_fac & CIF_ISP_DPCC_PG_FAC_RESERVED) ||
+            (method->rnd_thresh & CIF_ISP_DPCC_RND_THRESH_RESERVED) ||
+            (method->rg_fac & CIF_ISP_DPCC_RG_FAC_RESERVED)) {
             LOGE("%s:%d check error !", __FUNCTION__, __LINE__);
             goto err;
         }
         method++;
     }
+
     return 0;
 err:
     return -1;
@@ -383,7 +396,7 @@ XCamReturn rkisp1_check_params(struct rkisp1_isp_params_cfg* configs, int isp_ve
     int ret = 0;
 
     if (configs->module_cfg_update & CIFISP_MODULE_DPCC) {
-        ret = dpcc_param_check(&configs->others.dpcc_config);
+        ret = dpcc_param_check(&configs->others.dpcc_config, isp_ver);
         if (ret < 0)
             configs->module_cfg_update &= ~CIFISP_MODULE_DPCC;
     }
