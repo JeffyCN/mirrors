8dc5991543c556cfd0b0a322eed76486197acc8d
Eddie Cai <eddie.cai.linux@gmail.com>
2018-12-27 14:33:33 +0800

    add a simple script to test wifi
    Change-Id: Iedd350bb9bde91c4e1d1b6fa8953030dfcb89d9f
    Signed-off-by: Eddie Cai <eddie.cai.linux@gmail.com>

---
+++ wifi_start.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+WIFISSID=$1
+WIFIPWD=$2
+CONF=/userdata/cfg/wpa_supplicant.conf
+if [ ! -d /userdata/cfg ];then
+	mkdir /userdata/cfg
+fi
+
+cp /etc/wpa_supplicant.conf /userdata/cfg/
+echo "connect to WiFi ssid: $WIFISSID, Passwd: $WIFIPWD"
+sed -i "s/SSID/$WIFISSID/g" $CONF
+sed -i "s/PWD/$WIFIPWD/g" $CONF
+wpa_supplicant -B -i wlan0 -c $CONF
