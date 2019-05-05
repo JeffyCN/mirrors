fcfc8d2f0bda80d42f9688fa2732813862edcdee
Shunqian Zheng <zhengsq@rock-chips.com>
2019-05-05 18:51:03 +0800

    3a: update libs target for v2.0.0
    3A lib version:
       af:  v0.2.13
       awb: v0.0.b
       aec: v0.0.9
    
    Change-Id: Id850faf3f46ef51b454b724a8a505b639301f8f9
    Signed-off-by: Shunqian Zheng <zhengsq@rock-chips.com>

--- plugins/3a/rkiq/af/af.h
+++ plugins/3a/rkiq/af/af.h
@@ -92,9 +92,16 @@
 * 1) enable ALOGV/ALOGW for Android
 *v0.2.10
 * 1)remove afmCmdQue,so the system will must to be a sync thread.
+*v0.2.11
+* 1)remove the condition that trigger auto focus by window change.
+*v0.2.12
+* 1)fix the bug about touch focus.
+*v0.2.13
+* 1)fix the bug that AfmPosTbl init is wrog,so quick found will set a wrong position.
+* 2)fix the bug tha continue focus will trigger frequently.
 */
 
-#define CONFIG_AF_LIB_VERSION "v0.2.10"
+#define CONFIG_AF_LIB_VERSION "v0.2.13"
 
 #ifdef __cplusplus
 extern "C"
