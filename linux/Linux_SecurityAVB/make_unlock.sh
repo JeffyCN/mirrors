b5ce1e215f2dd5c0900461f675b8dbcd280e5cd9
zain wang <wzz@rock-chips.com>
2020-02-12 21:09:22 +0800

    tool: avb: add script to simplify AVB operation
    Change-Id: Ia09d3c6ef8e8bd2f170547299d849a76f9dd621d
    Signed-off-by: zain wang <wzz@rock-chips.com>

--- linux/Linux_SecurityAVB/make_unlock.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-python avb-challenge-verify.py raw_unlock_challenge.bin product_id.bin
-python avbtool make_atx_unlock_credential --output=unlock_credential.bin --intermediate_key_certificate=pik_certificate.bin --unlock_key_certificate=puk_certificate.bin --challenge=unlock_challenge.bin --unlock_key=testkey_puk.pem
