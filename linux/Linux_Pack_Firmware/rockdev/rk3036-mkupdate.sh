6d4d7635f6255f2bba3fa6c5b3b7f89c8b9b11aa
Hans Yang <yhx@rock-chips.com>
2020-03-16 11:44:50 +0800

    tools: mkupdate: correct rk3036g pack cpu info
    Signed-off-by: Hans Yang <yhx@rock-chips.com>
    Change-Id: Iaf689afb4cb09c6e825df659cf0ba326954b1cd5

--- linux/Linux_Pack_Firmware/rockdev/rk3036-mkupdate.sh
+++ linux/Linux_Pack_Firmware/rockdev/rk3036-mkupdate.sh
@@ -15,7 +15,7 @@ if [ ! -f "package-file" ]; then
 	exit 1
 fi
 ./afptool -pack ./ Image/update.img || pause
-./rkImageMaker -RK3036 Image/MiniLoaderAll.bin Image/update.img update.img -os_type:androidos || pause
+./rkImageMaker -RK303A Image/MiniLoaderAll.bin Image/update.img update.img -os_type:androidos || pause
 echo "Making update.img OK."
 #echo "Press any key to quit:"
 #read -n1 -s key
