7d32b00777fac44aac59c0b2061916054c68b0de
Jeffy Chen <jeffy.chen@rock-chips.com>
2020-06-19 17:00:59 +0800

    Revert "rockchipmpp: use max_reenc_times instead of skip_cnt to match mpp"
    This reverts commit 173a9f37f167d5b07155bf73a5ac3974a4a048cd.
    
    The max_reenc_times has nothing to do with skip_cnt.
    
    Change-Id: I2579bcf83a3d0fc6311e51be13803c44e5b6fc48
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

--- gst/rockchipmpp/gstmpph264enc.c
+++ gst/rockchipmpp/gstmpph264enc.c
@@ -84,7 +84,7 @@ gst_mpp_h264_enc_set_format (GstVideoEncoder * encoder,
   rc_cfg.fps_out_denorm = GST_VIDEO_INFO_FPS_D (&state->info);
   rc_cfg.gop = GST_VIDEO_INFO_FPS_N (&state->info)
       / GST_VIDEO_INFO_FPS_D (&state->info);
-  rc_cfg.max_reenc_times = 0;
+  rc_cfg.skip_cnt = 0;
 
   codec_cfg.h264.qp_init = 26;
 
--- gst/rockchipmpp/gstmppjpegenc.c
+++ gst/rockchipmpp/gstmppjpegenc.c
@@ -82,7 +82,7 @@ gst_mpp_jpeg_enc_set_format (GstVideoEncoder * encoder,
   rc_cfg.fps_out_denorm = GST_VIDEO_INFO_FPS_D (&state->info);
   rc_cfg.gop = GST_VIDEO_INFO_FPS_N (&state->info)
       / GST_VIDEO_INFO_FPS_D (&state->info);
-  rc_cfg.max_reenc_times = 0;
+  rc_cfg.skip_cnt = 0;
 
 
   if (mpp_video_enc->mpi->control (mpp_video_enc->mpp_ctx, MPP_ENC_SET_RC_CFG,
