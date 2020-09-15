9330b648224b22ed6f19e5840b76b6803e1a7aa9
Putin Lee <putin.li@rock-chips.com>
2020-09-15 17:41:17 +0800

    Modify meson.sh && cross file
    Signed-off-by: Putin Lee <putin.li@rock-chips.com>

--- meson.sh
+++ meson.sh
@@ -2,13 +2,13 @@ echo "# to rm ./build-rga/"
 rm -rf build-rga
 
 ORIGINAL_PATH=$PATH
-export PATH=/home/lee/rv1126/buildroot/output/rockchip_puma/host/bin:$PATH
+export PATH=/home/lee/new_rk3399_linux/rk3399/buildroot/output/rockchip_rk3399/host/bin:$PATH
 #export PATH=/home/lee/rk3399_linux/output/rockchip_rk3399/host/bin:$PATH
 
-export PKG_CONFIG_PATH=/home/lee/rv1126/buildroot/output/rockchip_puma/host/lib/pkgconfig:/home/lee/rv1126/buildroot/output/rockchip_puma/host/usr/aarch64-buildroot-linux-gnu/sysroot/usr/lib/pkgconfig
+export PKG_CONFIG_PATH=/home/lee/new_rk3399_linux/rk3399/buildroot/output/rockchip_rk3399/host/lib/pkgconfig:/home/lee/new_rk3399_linux/rk3399/buildroot/output/rockchip_rk3399/host/usr/aarch64-buildroot-linux-gnu/sysroot/usr/lib/pkgconfig
 #export PKG_CONFIG_PATH=/home/lee/rk3399_linux/output/rockchip_rk3399/host/lib/pkgconfig
 
-meson --prefix=/home/lee/rv1126/external/rga/output build-rga --cross-file cross/cross_file.txt
+meson --prefix=/home/lee/new_rk3399_linux/rk3399/external/rga/output build-rga --cross-file cross/cross_file.txt
 
 export PATH=$ORIGINAL_PATH
 
