dfadc663699bd49b1793f0a385b712bf886af905
Yao Xiao <xiaoyao@rock-chips.com>
2020-10-10 17:15:30 +0800

    Enhance tb_start_wifi.sh stability
    Signed-off-by: Yao Xiao <xiaoyao@rock-chips.com>
    Change-Id: Ibe247a2357a0c2d97b1c778bd4c570d0fe9b273a

--- tb_start_wifi.sh
+++ tb_start_wifi.sh
@@ -13,6 +13,9 @@ insmod_cnt=0
 check_cnt=0
 FORCE_CONFIG_WIFI=$3
 NEW_SSID=
+getdhcp_cnt=0
+connect_cnt=0
+wpa_cnt=0
 
 if [ -z $WIFISSID ]; then
 	echo -e "\033[33m WIFISSID is invalid, assume to Rockchip-guest \033[0m"
@@ -41,23 +44,51 @@ function getdhcp() {
 			echo 255 > /sys/class/leds/blue/brightness
 			break
 		fi
+
+		sleep 0.01
+
+		let getdhcp_cnt++
+		if [ $getdhcp_cnt -gt 3000 ]; then
+			echo "$NEW_SSID getdhcp failed!!!"
+			exit
+		fi
 	done
 }
 
 function check_wlan0() {
 	while true
 	do
+		lsmod | grep bcmdhd
+		if [ $? -ne 0 ]; then
+			echo "wait bcmdhd.ko ..."
+			sleep .2
+		else
+			echo "ismod bcmdhd.ko okay"
+			break
+		fi
+
+		let insmod_cnt++
+		if [ $insmod_cnt -gt 10 ]; then
+			echo "insmod failed !!!"
+			exit
+		fi
+	done
+
+	while true
+	do
 		ifconfig -a wlan0 | grep wlan0
 		if [ $? -ne 0 ]; then
 			echo "wait wlan0 ..."
-			sleep .1
+			sleep .2
 		else
 			echo "wlan0 exist"
 			break
 		fi
+
 		let check_cnt++
-		if [ $check_cnt -gt 2 ]; then
-			break
+		if [ $check_cnt -gt 10 ]; then
+			echo "check wlan0 failed!!!"
+			exit
 		fi
 	done
 }
@@ -84,26 +115,31 @@ function wlan_up() {
 		ifconfig wlan0 up
 		if [ $? -ne 0 ]; then
 			echo "wlan0 up failed"
-			sleep 0.1
+			sleep 0.2
 		else
 			echo "wlan0 up succeed"
 			break
 		fi
 
 		let up_cnt++
-		if [ $up_cnt -gt 2 ]; then
-			break
+		if [ $up_cnt -gt 10 ]; then
+			echo "wlan0 up failed!!!"
+			exit
 		fi
 	done
 }
 
-check_wlan0
-wlan_up
-tcpka_del
+function connect_wifi() {
 
 SSID=`dhd_priv isam_status | awk -F, '{print $5}' | sed -n '4p' | cut -d '"' -f 2`
 
 if [ "$SSID" ==  "" ] || [ "$FORCE_CONFIG_WIFI" == "true" ];then
+
+	if [ "$SSID" == "$WIFISSID" ];then
+		echo "$WIFISSID already connected"
+		return
+	fi
+
 	echo "WIFI INFO: $SSID, CONFIG: $FORCE_CONFIG_WIFI"
 	cp /etc/wpa_supplicant.conf /tmp/
 	echo "connect to WiFi ssid: $WIFISSID, Passwd: $WIFIPWD"
@@ -123,25 +159,42 @@ if [ "$SSID" ==  "" ] || [ "$FORCE_CONFIG_WIFI" == "true" ];then
 		if [ "$WPAPID" == "" ];then
 			sleep 0.5
 		else
+			echo "wpa_supplicant running"
 			break
 		fi
-	done
 
-	if [ "$SSID" !=  "" ] && [ "$FORCE_CONFIG_WIFI" == "true" ];then
-		echo "waiting $SSID disconnect ..."
-		sleep 2
-	fi
+		let wpa_cnt++
+		if [ $wpa_cnt -gt 10 ]; then
+			echo "wpa_supplicant up failed!!!"
+			exit
+		fi
+	done
 
 	while true
 	do
 		NEW_SSID=`dhd_priv isam_status | awk -F, '{print $5}' | sed -n '4p' | cut -d '"' -f 2`
-		if [ "$NEW_SSID" !=  "" ];then
-			echo "$NEW_SSID connecting..."
+		if [ "$NEW_SSID" ==  "" ];then
+			echo "$WIFISSID connecting..."
+			sleep 1
+		elif [ "$NEW_SSID" == "$SSID" ];then
+			echo "waiting $SSID disconnect ..."
+			sleep 1
+		else [ "$NEW_SSID" == "WIFISSID" ]
+			echo "$NEW_SSID connected to getdhcp"
 			break
 		fi
-	done
 
-	getdhcp
-else
-	getdhcp
+		let connect_cnt++
+		if [ $connect_cnt -gt 60 ]; then
+			echo "connect $NEW_SSID failed!!!"
+			exit
+		fi
+	done
 fi
+}
+
+check_wlan0
+wlan_up
+tcpka_del
+connect_wifi
+getdhcp
