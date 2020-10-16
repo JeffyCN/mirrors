8447b8af9c7eece7fd1ee6c80375609a7cd3e52b
Yu Qiaowei <cerf.yu@rock-chips.com>
2020-10-16 18:38:13 +0800

    Modify license and add file license.
    Signed-off-by: Yu Qiaowei <cerf.yu@rock-chips.com>
    Change-Id: I947e3db2fae7c8c2a3bc0f3fed05b6324184c804

--- samples/rgaROP/rgaRop.cpp
+++ samples/rgaROP/rgaRop.cpp
@@ -1,29 +1,20 @@
 /*
- * Copyright (C) 2018  Fuzhou Rockchip Electronics Co., Ltd. All rights reserved.
+ * Copyright (C) 2018  Rockchip Electronics Co., Ltd.
  * Authors:
  *     lihuang <putin.li@rock-chips.com>
  *     libin <bin.li@rock-chips.com>
  *
- * This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
- *  You should have received a copy of the GNU General Public License
- *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- * BY DOWNLOADING, INSTALLING, COPYING, SAVING OR OTHERWISE USING THIS SOFTWARE,
- * YOU ACKNOWLEDGE THAT YOU AGREE THE SOFTWARE RECEIVED FORM ROCKCHIP IS PROVIDED
- * TO YOU ON AN "AS IS" BASIS and ROCKCHP DISCLAIMS ANY AND ALL WARRANTIES AND
- * REPRESENTATIONS WITH RESPECT TO SUCH FILE, WHETHER EXPRESS, IMPLIED, STATUTORY
- * OR OTHERWISE, INCLUDING WITHOUT LIMITATION, ANY IMPLIED WARRANTIES OF TITLE,
- * NON-INFRINGEMENT, MERCHANTABILITY, SATISFACTROY QUALITY, ACCURACY OR FITNESS FOR
- * A PARTICULAR PURPOSE.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 #define LOG_NDEBUG 0
 #define LOG_TAG "rgaBlit"
