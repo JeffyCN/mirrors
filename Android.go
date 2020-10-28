aa0520e56a5461760d144ff693de38020214757d
Li Huang <putin.li@rock-chips.com>
2020-10-28 17:20:59 +0800

    Update:
    1.add dependencies for platfrom_gralloc4
    2.remove unuse compile log on Android.go
    
    Signed-off-by: Li Huang <putin.li@rock-chips.com>
    Change-Id: I37394f0cbaf8efcf50040fd331aa1637301722f3
    Signed-off-by: Li Huang <putin.li@rock-chips.com>

--- Android.go
+++ Android.go
@@ -3,13 +3,10 @@ package librga
 import (
         "android/soong/android"
         "android/soong/cc"
-        "fmt"
         "strings"
 )
 
 func init() {
-	//该打印会在执行mm命令时，打印在屏幕上
-	fmt.Println("librga want to conditional Compile")
 	android.RegisterModuleType("cc_librga", DefaultsFactory)
 }
 
