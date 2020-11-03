47c22fcdf04bd4333080272bf7874bd6d2578a97
Bian Jin chen <kenjc.bian@rock-chips.com>
2020-11-03 08:47:19 +0800

    Android: only use gralloc 4 when TARGET_RK_GRALLOC_VERSION=4
    Signed-off-by: Bian Jin chen <kenjc.bian@rock-chips.com>
    Change-Id: I82d85641fd1d632ea5d2667d4eecbbe53c98dc26

--- Android.go
+++ Android.go
@@ -7,7 +7,7 @@ import (
 )
 
 func init() {
-	android.RegisterModuleType("cc_librga", DefaultsFactory)
+    android.RegisterModuleType("cc_librga", DefaultsFactory)
 }
 
 func DefaultsFactory() (android.Module) {
@@ -18,41 +18,41 @@ func DefaultsFactory() (android.Module) {
 
 func Defaults(ctx android.LoadHookContext) {
     type props struct {
-		Srcs []string
+        Srcs []string
         Cflags []string
-		Shared_libs []string
-		Include_dirs []string
+        Shared_libs []string
+        Include_dirs []string
     }
 
     p := &props{}
-	p.Srcs = getSrcs(ctx)
+    p.Srcs = getSrcs(ctx)
     p.Cflags = getCflags(ctx)
-	p.Shared_libs = getSharedLibs(ctx)
-	p.Include_dirs = getIncludeDirs(ctx)
+    p.Shared_libs = getSharedLibs(ctx)
+    p.Include_dirs = getIncludeDirs(ctx)
 
     ctx.AppendProperties(p)
 }
 
 //条件编译主要修改函数
 func getCflags(ctx android.BaseContext) ([]string) {
-	var cppflags []string
+    var cppflags []string
 
-	sdkVersion := ctx.AConfig().PlatformSdkVersionInt()
+    sdkVersion := ctx.AConfig().PlatformSdkVersionInt()
 
-	//该打印输出为: TARGET_PRODUCT:rk3328 fmt.Println("TARGET_PRODUCT:",ctx.AConfig().Getenv("TARGET_PRODUCT")) //通过 strings.EqualFold 比较字符串，可参考go语言字符串对比
-	if (strings.EqualFold(ctx.AConfig().Getenv("TARGET_BOARD_PLATFORM"),"rk3368") ) {
-	//添加 DEBUG 宏定义
+    //该打印输出为: TARGET_PRODUCT:rk3328 fmt.Println("TARGET_PRODUCT:",ctx.AConfig().Getenv("TARGET_PRODUCT")) //通过 strings.EqualFold 比较字符串，可参考go语言字符串对比
+    if (strings.EqualFold(ctx.AConfig().Getenv("TARGET_BOARD_PLATFORM"),"rk3368") ) {
+    //添加 DEBUG 宏定义
         cppflags = append(cppflags,"-DRK3368=1")
-	}
-
-	if (strings.EqualFold(ctx.AConfig().Getenv("TARGET_BOARD_PLATFORM"),"rk3326") ) {
-		if (sdkVersion >= 30 ) {
-			cppflags = append(cppflags,"-DUSE_GRALLOC_4")
-		}
     }
 
-	//将需要区分的环境变量在此区域添加 //....
-	return cppflags
+    if (strings.EqualFold(ctx.AConfig().Getenv("TARGET_RK_GRALLOC_VERSION"),"4") ) {
+        if (sdkVersion >= 30 ) {
+            cppflags = append(cppflags,"-DUSE_GRALLOC_4")
+        }
+    }
+
+    //将需要区分的环境变量在此区域添加 //....
+    return cppflags
 }
 
 func getSharedLibs(ctx android.BaseContext) ([]string) {
@@ -60,11 +60,11 @@ func getSharedLibs(ctx android.BaseContext) ([]string) {
 
     sdkVersion := ctx.AConfig().PlatformSdkVersionInt()
 
-    if (strings.EqualFold(ctx.AConfig().Getenv("TARGET_BOARD_PLATFORM"),"rk3326") ) {
+    if (strings.EqualFold(ctx.AConfig().Getenv("TARGET_RK_GRALLOC_VERSION"),"4") ) {
         if (sdkVersion >= 30 ) {
             libs = append(libs, "libgralloctypes")
             libs = append(libs, "libhidlbase")
-			libs = append(libs, "android.hardware.graphics.mapper@4.0")
+            libs = append(libs, "android.hardware.graphics.mapper@4.0")
         }
     }
 
@@ -76,10 +76,10 @@ func getIncludeDirs(ctx android.BaseContext) ([]string) {
 
     sdkVersion := ctx.AConfig().PlatformSdkVersionInt()
 
-    if (strings.EqualFold(ctx.AConfig().Getenv("TARGET_BOARD_PLATFORM"),"rk3326") ) {
+    if (strings.EqualFold(ctx.AConfig().Getenv("TARGET_RK_GRALLOC_VERSION"),"4") ) {
         if (sdkVersion >= 30 ) {
-			dirs = append(dirs, "hardware/rockchip/libgralloc/bifrost")
-			dirs = append(dirs, "hardware/rockchip/libgralloc/bifrost/src")
+            dirs = append(dirs, "hardware/rockchip/libgralloc/bifrost")
+            dirs = append(dirs, "hardware/rockchip/libgralloc/bifrost/src")
         }
     }
 
@@ -91,7 +91,7 @@ func getSrcs(ctx android.BaseContext) ([]string) {
 
     sdkVersion := ctx.AConfig().PlatformSdkVersionInt()
 
-    if (strings.EqualFold(ctx.AConfig().Getenv("TARGET_BOARD_PLATFORM"),"rk3326") ) {
+    if (strings.EqualFold(ctx.AConfig().Getenv("TARGET_RK_GRALLOC_VERSION"),"4") ) {
         if (sdkVersion >= 30 ) {
             src = append(src, "core/platform_gralloc4.cpp")
         }
