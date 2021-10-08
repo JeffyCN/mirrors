c82e50f82c62ee640e5d15c4946c6e61a6d1f5b0
Yu Qiaowei <cerf.yu@rock-chips.com>
2021-10-08 15:25:08 +0800

    build: cmake support compiling with 'buildroot' TARGET.
    version: 1.3.0_[10]
    
    Signed-off-by: Yu Qiaowei <cerf.yu@rock-chips.com>
    Change-Id: If1e840010454185128ff44219f9a587d8a10a8d2

--- cmake-android.sh
+++ cmake-android.sh
@@ -3,7 +3,7 @@
 ANDROID_NDK_HOME=/Users/jacobchen/Library/Android/sdk/ndk/21.3.6528147
 CMAKE_ANDROID=/Users/jacobchen/Library/Android/sdk/cmake/3.6.4111459/bin/cmake
 
-${CMAKE_ANDROID} -DCMAKE_BUILD_TARGET=ndk -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK_HOME/build/cmake/android.toolchain.cmake \
+${CMAKE_ANDROID} -DCMAKE_BUILD_TARGET=android_ndk -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK_HOME/build/cmake/android.toolchain.cmake \
       -DANDROID_NDK=$ANDROID_NDK_HOME \
       -DANDROID_ABI=armeabi-v7a \
       -DANDROID_TOOLCHAIN=clang \
