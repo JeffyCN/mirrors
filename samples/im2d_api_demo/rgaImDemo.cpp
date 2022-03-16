10fc30d6329c833770d5188166d5aedfa271eb46
Yu Qiaowei <cerf.yu@rock-chips.com>
2022-03-16 16:36:12 +0800

    samples: Add error judgment about handle
    version: 1.7.3_[5]
    
    Signed-off-by: Yu Qiaowei <cerf.yu@rock-chips.com>
    Change-Id: I56d1e1d603d134797c1a5d119a8056621e57ab84

--- samples/im2d_api_demo/rgaImDemo.cpp
+++ samples/im2d_api_demo/rgaImDemo.cpp
@@ -239,7 +239,15 @@ int main(int argc, char*  argv[]) {
         }
 
 		src_handle = importbuffer_AHardwareBuffer(src_buf);
+		if (src_handle <= 0) {
+			printf("Failed to import AHardwareBuffer for src channel!\n");
+			return ERROR;
+		}
 		dst_handle = importbuffer_AHardwareBuffer(dst_buf);
+		if (dst_handle <= 0) {
+			printf("Failed to import AHardwareBuffer for dst channel!\n");
+			return ERROR;
+		}
 #else
         src_buf = GraphicBuffer_Init(SRC_WIDTH, SRC_HEIGHT, SRC_FORMAT);
         dst_buf = GraphicBuffer_Init(DST_WIDTH, DST_HEIGHT, DST_FORMAT);
@@ -265,7 +273,15 @@ int main(int argc, char*  argv[]) {
         }
 
 		src_handle = importbuffer_GraphicBuffer(src_buf);
+		if (src_handle <= 0) {
+			printf("Failed to import GraphicBuffer for src channel!\n");
+			return ERROR;
+		}
 		dst_handle = importbuffer_GraphicBuffer(dst_buf);
+		if (dst_handle <= 0) {
+			printf("Failed to import GraphicBuffer for dst channel!\n");
+			return ERROR;
+		}
 #endif
 #elif LINUX
         src_buf = (char*)malloc(SRC_WIDTH*SRC_HEIGHT*get_bpp_from_format(SRC_FORMAT));
@@ -288,7 +304,15 @@ int main(int argc, char*  argv[]) {
         }
 
 		src_handle = importbuffer_virtualaddr(src_buf, SRC_WIDTH, SRC_HEIGHT, SRC_FORMAT);
+		if (src_handle <= 0) {
+			printf("Failed to import virtualaddr for src channel!\n");
+			return ERROR;
+		}
 		dst_handle = importbuffer_virtualaddr(dst_buf, DST_WIDTH, DST_HEIGHT, DST_FORMAT);
+		if (dst_handle <= 0) {
+			printf("Failed to import virtualaddr for dst channel!\n");
+			return ERROR;
+		}
 #endif
 
 		src = wrapbuffer_handle(src_handle, SRC_WIDTH, SRC_HEIGHT, SRC_FORMAT);
@@ -352,6 +376,10 @@ int main(int argc, char*  argv[]) {
 	                    }
 
 						dst_handle = importbuffer_AHardwareBuffer(dst_buf);
+						if (dst_handle <= 0) {
+							printf("Failed to import AHardwareBuffer for dst channel!\n");
+							return ERROR;
+						}
 #else
 	                    dst_buf = GraphicBuffer_Init(1920, 1080, DST_FORMAT);
 	                    if (dst_buf == NULL) {
@@ -364,6 +392,10 @@ int main(int argc, char*  argv[]) {
 	                    }
 
 						dst_handle = importbuffer_GraphicBuffer(dst_buf);
+						if (dst_handle <= 0) {
+							printf("Failed to import GraphicBuffer for dst channel!\n");
+							return ERROR;
+						}
 #endif
 #elif LINUX
 	                    if (dst_buf != NULL) {
@@ -375,6 +407,10 @@ int main(int argc, char*  argv[]) {
 	                    memset(dst_buf,0x00,1920*1080*get_bpp_from_format(DST_FORMAT));
 
 						dst_handle = importbuffer_virtualaddr(dst_buf, 1920, 1080, DST_FORMAT);
+						if (dst_handle <= 0) {
+							printf("Failed to import virtualaddr for dst channel!\n");
+							return ERROR;
+						}
 #endif
 	                    dst = wrapbuffer_handle(dst_handle, 1920, 1080, DST_FORMAT);
 	                    if(dst.width == 0) {
@@ -398,6 +434,10 @@ int main(int argc, char*  argv[]) {
 	                    }
 
 	                    dst_handle = importbuffer_AHardwareBuffer(dst_buf);
+						if (dst_handle <= 0) {
+							printf("Failed to import AHardwareBuffer for dst channel!\n");
+							return ERROR;
+						}
 #else
 	                    dst_buf = GraphicBuffer_Init(720, 480, DST_FORMAT);
 	                    if (dst_buf == NULL) {
@@ -410,6 +450,10 @@ int main(int argc, char*  argv[]) {
 	                    }
 
 	                    dst_handle = importbuffer_GraphicBuffer(dst_buf);
+						if (dst_handle <= 0) {
+							printf("Failed to import GraphicBuffer for dst channel!\n");
+							return ERROR;
+						}
 #endif
 #elif LINUX
 	                    if (dst_buf != NULL) {
@@ -421,6 +465,10 @@ int main(int argc, char*  argv[]) {
 	                    memset(dst_buf,0x00,720*480*get_bpp_from_format(DST_FORMAT));
 
 	                    dst_handle = importbuffer_virtualaddr(dst_buf, 720, 480, DST_FORMAT);
+						if (dst_handle <= 0) {
+							printf("Failed to import virtualaddr for dst channel!\n");
+							return ERROR;
+						}
 #endif
 	                    dst = wrapbuffer_handle(dst_handle, 720, 480, DST_FORMAT);
 	                    if(dst.width == 0) {
