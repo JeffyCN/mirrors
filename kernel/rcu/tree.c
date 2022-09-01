63eb826ccac0f4fd2cdf4a9c19e3c436ac619c70
Huibin Hong <huibin.hong@rock-chips.com>
2022-09-01 02:36:47 +0000

    rcu: add Kconfig options for sysctl_panic_on_rcu_stall
    Signed-off-by: Huibin Hong <huibin.hong@rock-chips.com>
    Change-Id: Iaae5a7607f0d8c4537676c5f663eefdc0c51848d

--- kernel/rcu/tree.c
+++ kernel/rcu/tree.c
@@ -127,7 +127,7 @@ int rcu_num_lvls __read_mostly = RCU_NUM_LVLS;
 int num_rcu_lvl[] = NUM_RCU_LVL_INIT;
 int rcu_num_nodes __read_mostly = NUM_RCU_NODES; /* Total # rcu_nodes in use. */
 /* panic() on RCU Stall sysctl. */
-int sysctl_panic_on_rcu_stall __read_mostly;
+int sysctl_panic_on_rcu_stall __read_mostly = CONFIG_BOOTPARAM_RCU_STALL_PANIC_VALUE;
 ATOMIC_NOTIFIER_HEAD(rcu_stall_notifier_list);
 
 /*
