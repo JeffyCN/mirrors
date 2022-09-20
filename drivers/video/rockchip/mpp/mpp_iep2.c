36167a05b7757c1b6babb679fb6d8323cd16751f
Ding Wei <leo.ding@rock-chips.com>
2022-09-20 15:54:29 +0800

    video: rockchip: mpp: iep2: Add work mode setting
    Change-Id: I46c772e5c8608d8c7aa7e3bec0331da1af4832d3
    Signed-off-by: Ding Wei <leo.ding@rock-chips.com>

--- drivers/video/rockchip/mpp/mpp_iep2.c
+++ drivers/video/rockchip/mpp/mpp_iep2.c
@@ -368,6 +368,8 @@ static void iep2_config(struct mpp_dev *mpp, struct iep_task *task)
 		| IEP2_REG_DEBUG_DATA_EN;
 	mpp_write_relaxed(mpp, IEP2_REG_IEP_CONFIG0, reg);
 
+	mpp_write_relaxed(mpp, IEP2_REG_WORK_MODE, IEP2_REG_IEP2_MODE);
+
 	reg = IEP2_REG_SRC_PIC_WIDTH(width - 1)
 		| IEP2_REG_SRC_PIC_HEIGHT(height - 1);
 	mpp_write_relaxed(mpp, IEP2_REG_SRC_IMG_SIZE, reg);
