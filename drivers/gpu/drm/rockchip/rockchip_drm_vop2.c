8b7d7d377308adb244828c8ec0d6d3453839c532
Andy Yan <andy.yan@rock-chips.com>
2022-09-28 19:52:05 +0800

    drm/rockchip: vop2: Restore lut after VP exit standby
    vop2_crtc_load_lut need to sync with vysnc, so we should
    make sure the VP exit standby and setting the right dclk,
    or we will meet a cfg done timeout:
    
    [drm:vop2_wait_for_fs_by_done_bit_status] *ERROR* wait vp1 done bit
    status timeout, vcnt: 0
    
    Fixes: 5aa1d0e08f49 ("drm/rockchip: vop2: fix double config done at one
    frame time")
    
    Change-Id: I66d1e7ba260dc376c7f7ce132dad2212649b31e8
    Signed-off-by: Andy Yan <andy.yan@rock-chips.com>

--- drivers/gpu/drm/rockchip/rockchip_drm_vop2.c
+++ drivers/gpu/drm/rockchip/rockchip_drm_vop2.c
@@ -4423,11 +4423,6 @@ static void vop2_crtc_atomic_enable(struct drm_crtc *crtc, struct drm_crtc_state
 	vcstate->mode_update = vop2_crtc_mode_update(crtc);
 	if (vcstate->mode_update)
 		vop2_disable_all_planes_for_crtc(crtc);
-	/*
-	 * restore the lut table.
-	 */
-	if (vp->gamma_lut_active)
-		vop2_crtc_load_lut(crtc);
 
 	dclk_inv = (vcstate->bus_flags & DRM_BUS_FLAG_PIXDATA_DRIVE_NEGEDGE) ? 1 : 0;
 	val = (adjusted_mode->flags & DRM_MODE_FLAG_NHSYNC) ? 0 : BIT(HSYNC_POSITIVE);
@@ -4640,6 +4635,12 @@ static void vop2_crtc_atomic_enable(struct drm_crtc *crtc, struct drm_crtc_state
 	VOP_MODULE_SET(vop2, vp, standby, 0);
 
 	drm_crtc_vblank_on(crtc);
+
+	/*
+	 * restore the lut table.
+	 */
+	if (vp->gamma_lut_active)
+		vop2_crtc_load_lut(crtc);
 
 	vop2_unlock(vop2);
 }
