496ccda2d2c40fc21773e59497343b518cd58fa6
Jianlong Wang <jianlong.wang@rock-chips.com>
2022-10-02 10:27:43 +0800

    rockdev: rk3358j/rk3568j: support package yx extra partitions
    Signed-off-by: Jianlong Wang <jianlong.wang@rock-chips.com>
    Change-Id: I6229f88cfa3a3da0202c231d05c342894a750a8c

---
+++ linux/Linux_Pack_Firmware/rockdev/rk3568J-mkupdate.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+pause()
+{
+echo "Press any key to quit:"
+read -n1 -s key
+exit 1
+}
+echo "start to make updateall.img..."
+if [ ! -f "Image/parameter" -a ! -f "Image/parameter.txt" ]; then
+	echo "Error:No found parameter!"
+	exit 1
+fi
+if [ ! -f "package-file" ]; then
+	echo "Error:No found package-file!"
+	exit 1
+fi
+./afptool -pack ./ Image/updateall.img || pause
+./rkImageMaker -RK3568 Image/MiniLoaderAll.bin Image/updateall.img updateall.img -os_type:androidos || pause
+echo "Making ./Image/updateall.img OK."
+#echo "Press any key to quit:"
+#read -n1 -s key
+exit $?
