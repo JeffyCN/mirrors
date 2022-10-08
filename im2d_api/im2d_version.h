b9817e278d29162baca8020af41efbc870d169f2
Yu Qiaowei <cerf.yu@rock-chips.com>
2022-10-08 17:27:18 +0800

    im2d_api: version: add macro to quickly set rga_api version
    Signed-off-by: Yu Qiaowei <cerf.yu@rock-chips.com>
    Change-Id: Ic7e8166266c9ea4a78a230e83371e4123aead062

--- im2d_api/im2d_version.h
+++ im2d_api/im2d_version.h
@@ -35,11 +35,21 @@
     RGA_VERSION_STR(RGA_API_BUILD_VERSION) "]"
 #define RGA_API_FULL_VERSION "rga_api version " RGA_API_VERSION
 
-#define RGA_SET_CURRENT_API_VERISON (\
+/* For header file version verification */
+#define RGA_SET_CURRENT_API_HEADER_VERISON (\
     (RGA_API_MAJOR_VERSION & 0xff) << 24 | \
     (RGA_API_MINOR_VERSION & 0xff) << 16 | \
     (RGA_API_REVISION_VERSION & 0xff) << 8 | \
     (RGA_API_BUILD_VERSION & 0xff)\
+    )
+#define RGA_SET_CURRENT_API_VERISON RGA_SET_CURRENT_API_HEADER_VERISON
+
+#define RGA_CURRENT_API_HEADER_VERSION ( \
+    (struct rga_version_t){ \
+    RGA_API_MAJOR_VERSION, \
+    RGA_API_MINOR_VERSION, \
+    RGA_API_REVISION_VERSION, \
+    RGA_API_VERSION} \
     )
 
 #endif /* _RGA_IM2D_VERSION_H_ */
