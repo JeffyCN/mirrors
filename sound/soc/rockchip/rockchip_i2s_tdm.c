1ce1e5f5d326fbf67b9be9989188b7708e4d73e6
Sugar Zhang <sugar.zhang@rock-chips.com>
2022-10-09 14:31:32 +0800

    ASoC: rockchip: i2s-tdm: Fix FIFO clear error on SLAVE mode
    Workaround for FIFO clear on SLAVE mode:
    
    A Suggest to do reset hclk domain and then do mclk
      domain, especially for SLAVE mode without CLK in.
      at last, recovery regmap config.
    
    B Suggest to switch to MASTER, and then do FIFO clr,
      at last, bring back to SLAVE.
    
    Now we choose plan B here.
    
    Signed-off-by: Sugar Zhang <sugar.zhang@rock-chips.com>
    Change-Id: Iac775ff092c6d16e0240459e60fc8112b54f61c6

--- sound/soc/rockchip/rockchip_i2s_tdm.c
+++ sound/soc/rockchip/rockchip_i2s_tdm.c
@@ -383,9 +383,6 @@ static int rockchip_i2s_tdm_clear(struct rk_i2s_tdm_dev *i2s_tdm,
 	unsigned int val = 0;
 	int ret = 0;
 
-	if (!i2s_tdm->is_master_mode)
-		goto reset;
-
 	switch (clr) {
 	case I2S_CLR_TXC:
 		rst = i2s_tdm->tx_reset;
@@ -399,11 +396,31 @@ static int rockchip_i2s_tdm_clear(struct rk_i2s_tdm_dev *i2s_tdm,
 		return -EINVAL;
 	}
 
+	/*
+	 * Workaround for FIFO clear on SLAVE mode:
+	 *
+	 * A Suggest to do reset hclk domain and then do mclk
+	 *   domain, especially for SLAVE mode without CLK in.
+	 *   at last, recovery regmap config.
+	 *
+	 * B Suggest to switch to MASTER, and then do FIFO clr,
+	 *   at last, bring back to SLAVE.
+	 *
+	 * Now we choose plan B here.
+	 */
+	if (!i2s_tdm->is_master_mode)
+		regmap_update_bits(i2s_tdm->regmap, I2S_CKR,
+				   I2S_CKR_MSS_MASK, I2S_CKR_MSS_MASTER);
 	regmap_update_bits(i2s_tdm->regmap, I2S_CLR, clr, clr);
 	ret = regmap_read_poll_timeout_atomic(i2s_tdm->regmap, I2S_CLR, val,
 					      !(val & clr), 10, 100);
+	if (!i2s_tdm->is_master_mode)
+		regmap_update_bits(i2s_tdm->regmap, I2S_CKR,
+				   I2S_CKR_MSS_MASK, I2S_CKR_MSS_SLAVE);
+
 	if (ret < 0) {
-		dev_warn(i2s_tdm->dev, "failed to clear %u\n", clr);
+		dev_warn(i2s_tdm->dev, "failed to clear %u on %s mode\n",
+			 clr, i2s_tdm->is_master_mode ? "master" : "slave");
 		goto reset;
 	}
 
@@ -1222,24 +1239,22 @@ static int rockchip_i2s_tdm_hw_params(struct snd_pcm_substream *substream,
 	dma_data = snd_soc_dai_get_dma_data(dai, substream);
 	dma_data->maxburst = MAXBURST_PER_FIFO * params_channels(params) / 2;
 
-	if (i2s_tdm->is_master_mode) {
-		if (i2s_tdm->mclk_calibrate)
-			rockchip_i2s_tdm_calibrate_mclk(i2s_tdm, substream,
-							params_rate(params));
+	if (i2s_tdm->mclk_calibrate)
+		rockchip_i2s_tdm_calibrate_mclk(i2s_tdm, substream,
+						params_rate(params));
 
-		ret = rockchip_i2s_tdm_set_mclk(i2s_tdm, substream, &mclk);
-		if (ret)
-			goto err;
+	ret = rockchip_i2s_tdm_set_mclk(i2s_tdm, substream, &mclk);
+	if (ret)
+		goto err;
 
-		mclk_rate = clk_get_rate(mclk);
-		bclk_rate = i2s_tdm->bclk_fs * params_rate(params);
-		if (!bclk_rate) {
-			ret = -EINVAL;
-			goto err;
-		}
-		div_bclk = DIV_ROUND_CLOSEST(mclk_rate, bclk_rate);
-		div_lrck = bclk_rate / params_rate(params);
+	mclk_rate = clk_get_rate(mclk);
+	bclk_rate = i2s_tdm->bclk_fs * params_rate(params);
+	if (!bclk_rate) {
+		ret = -EINVAL;
+		goto err;
 	}
+	div_bclk = DIV_ROUND_CLOSEST(mclk_rate, bclk_rate);
+	div_lrck = bclk_rate / params_rate(params);
 
 	switch (params_format(params)) {
 	case SNDRV_PCM_FORMAT_S8:
