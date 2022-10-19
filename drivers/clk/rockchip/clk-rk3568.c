b5abc0112d5660e0f97fe8aa6695a346181d9ddd
Elaine Zhang <zhangqing@rock-chips.com>
2022-10-19 10:40:08 +0800

    clk: rockchip: rk3568: mark pclk_usb as critical
    Change-Id: I9f7ab4a775855f2c1471c0304e4e08bf13fe3629
    Signed-off-by: Elaine Zhang <zhangqing@rock-chips.com>

--- drivers/clk/rockchip/clk-rk3568.c
+++ drivers/clk/rockchip/clk-rk3568.c
@@ -1652,6 +1652,7 @@ static const char *const rk3568_cru_critical_clocks[] __initconst = {
 	"hclk_php",
 	"pclk_php",
 	"hclk_usb",
+	"pclk_usb",
 };
 
 static const char *const rk3568_pmucru_critical_clocks[] __initconst = {
