JeffyCN/mirrors

commit a5930b4e272d575230baabc195994deab168877f

usb: dwc2: Fix device and host abnormal usb linestate
Some Rockchip SoCs (e.g rk3308bs) usb linestate maybe
in abnormal status, and cause usb enumeration fail.
This patch add a hrtimer for dwc2 device mode and add
a delay work for dwc2 host mode to check the linestate
periodically, and if the usb linestate is in abnormal
status, the software will control the usb phy to drive
dp/dm in normal voltage, and make sure the high-speed
handshake successful.

Signed-off-by: William Wu <william.wu@rock-chips.com>
Change-Id: I15a1ad51f2975869fe377fdb580d00c5213d00d3

drivers/usb/dwc2/gadget.c
@@ -31,6 +31,7 @@
 #include <linux/usb/ch9.h>
 #include <linux/usb/gadget.h>
 #include <linux/usb/phy.h>
+#include <linux/rockchip/cpu.h>
 
 #include "core.h"
 #include "hw.h"
@@ -2531,8 +2532,17 @@ static void dwc2_hsotg_irq_enumdone(struct dwc2_hsotg *hsotg)
 		 */
 		break;
 	}
-	dev_info(hsotg->dev, "new device is %s\n",
-		 usb_speed_string(hsotg->gadget.speed));
+
+	if ((hsotg->set_linestate != PHY_SET_USB_NONE) &&
+	    (hsotg->gadget.speed == USB_SPEED_FULL)) {
+		dev_dbg(hsotg->dev, "EnumDone FS in linestate %d\n",
+			hsotg->set_linestate);
+		hsotg->gadget.speed = USB_SPEED_UNKNOWN;
+		goto out;
+	} else {
+		dev_dbg(hsotg->dev, "new device is %s\n",
+			usb_speed_string(hsotg->gadget.speed));
+	}
 
 	/*
 	 * we should now know the maximum packet size for an
@@ -2556,6 +2566,7 @@ static void dwc2_hsotg_irq_enumdone(struct dwc2_hsotg *hsotg)
 
 	dwc2_hsotg_enqueue_setup(hsotg);
 
+out:
 	dev_dbg(hsotg->dev, "EP0: DIEPCTL0=0x%08x, DOEPCTL0=0x%08x\n",
 		dwc2_readl(hsotg->regs + DIEPCTL0),
 		dwc2_readl(hsotg->regs + DOEPCTL0));
@@ -2622,6 +2633,7 @@ void dwc2_hsotg_disconnect(struct dwc2_hsotg *hsotg)
 
 	call_gadget(hsotg, disconnect);
 	hsotg->lx_state = DWC2_L3;
+	hsotg->gadget.speed = USB_SPEED_UNKNOWN;
 
 	usb_gadget_set_state(&hsotg->gadget, USB_STATE_NOTATTACHED);
 }
@@ -2826,8 +2838,20 @@ static void dwc2_hsotg_core_disconnect(struct dwc2_hsotg *hsotg)
 
 void dwc2_hsotg_core_connect(struct dwc2_hsotg *hsotg)
 {
+	ktime_t delay;
+
 	/* remove the soft-disconnect and let's go */
 	__bic32(hsotg->regs + DCTL, DCTL_SFTDISCON);
+
+	if (usb_linestate_is_enabled()) {
+		/* Disable software to own the fs transceiver */
+		phy_set_linestate(hsotg->phy, PHY_SET_USB_DONE);
+		hsotg->set_linestate = PHY_SET_USB_NONE;
+		hsotg->wait_enum_count = 0;
+		delay = ktime_set(0, 5 * NSEC_PER_MSEC);
+		hrtimer_start(&hsotg->check_linestate_timer, delay,
+			      HRTIMER_MODE_REL);
+	}
 }
 
 /**
@@ -4019,6 +4043,152 @@ static void dwc2_wait_reset_watchdog(unsigned long data)
 	spin_unlock_irqrestore(&hsotg->lock, flags);
 }
 
+#define DM_IS_HIGH			0x2
+#define DP_DM_IS_HIGH			0x3
+#define RETRY_CNT			20
+#define DWC2_HRTIMER_WAIT_SE0		(5 * NSEC_PER_MSEC)
+#define DWC2_HRTIMER_LS_RESUME		(10 * NSEC_PER_MSEC)
+#define DWC2_HRTIMER_DIS		(15 * NSEC_PER_MSEC)
+#define DWC2_HRTIMER_FORCE_DP_H(ms)	(ms * NSEC_PER_MSEC)
+#define DWC2_HRTIMER_POLL		(2000 * NSEC_PER_MSEC)
+
+static enum hrtimer_restart dwc2_check_linestate_timer_fn(struct hrtimer *t)
+{
+	enum hrtimer_restart ret;
+	struct dwc2_hsotg *hsotg = container_of(t, struct dwc2_hsotg,
+						check_linestate_timer);
+	int linestate, enum_speed, max_speed;
+	ktime_t delay;
+	u32 dsts, dcfg, dctl;
+	static int retry = RETRY_CNT;
+	static unsigned long wait_conn_delay = DWC2_HRTIMER_FORCE_DP_H(100);
+
+	if (!dwc2_is_device_mode(hsotg)) {
+		dev_dbg(hsotg->dev, "currently in Host mode, do nothing in device hrtimer\n");
+		return HRTIMER_NORESTART;
+	}
+
+	dsts = dwc2_readl(hsotg->regs + DSTS);
+	dcfg = dwc2_readl(hsotg->regs + DCFG);
+	dctl = dwc2_readl(hsotg->regs + DCTL);
+	enum_speed = (dsts & DSTS_ENUMSPD_MASK) >> DSTS_ENUMSPD_SHIFT;
+	max_speed = (dcfg & DCFG_DEVSPD_MASK) >> DCFG_DEVSPD_SHIFT;
+
+	switch (hsotg->set_linestate) {
+	case PHY_SET_USB_NONE:
+		linestate = phy_get_linestate(hsotg->phy);
+		dev_dbg(hsotg->dev, "linestate 0x%08x dsts 0x%08x, dcfg 0x%08x\n",
+			linestate, dsts, dcfg);
+		if ((!(dctl & DCTL_SFTDISCON)) &&
+		    (max_speed == DCFG_DEVSPD_HS) &&
+		    ((enum_speed != DSTS_ENUMSPD_HS) ||
+		     (linestate == DP_DM_IS_HIGH))) {
+			if (((linestate & DM_IS_HIGH) && (!hsotg->connected)) ||
+			    (hsotg->wait_enum_count >= 2)) {
+				pr_warn_once("usb linestate error!\n");
+				phy_set_linestate(hsotg->phy,
+						  PHY_SET_USB_DP_H_DM_L);
+				hsotg->set_linestate = PHY_SET_USB_DP_H_DM_L;
+				hsotg->wait_enum_count = 0;
+				retry = RETRY_CNT;
+				wait_conn_delay = DWC2_HRTIMER_FORCE_DP_H(100);
+				delay = ktime_set(0, DWC2_HRTIMER_LS_RESUME);
+			} else if (!(linestate & DM_IS_HIGH) &&
+				   (!hsotg->connected) &&
+				   (hsotg->gadget.speed != USB_SPEED_UNKNOWN)) {
+				/* Maybe DM Voltage in unexpected high level */
+				pr_warn_once("Maybe DM Volt in high level\n");
+				hsotg->wait_enum_count++;
+				delay = ktime_set(0, DWC2_HRTIMER_POLL);
+			} else {
+				delay = ktime_set(0, DWC2_HRTIMER_POLL);
+			}
+		} else {
+			delay = ktime_set(0, DWC2_HRTIMER_POLL);
+		}
+
+		break;
+	case PHY_SET_USB_DP_H_DM_L:
+		phy_set_linestate(hsotg->phy, PHY_SET_USB_DISC);
+		hsotg->set_linestate = PHY_SET_USB_DISC;
+		/*
+		 * Delay at least 15ms to wait phy hardware to
+		 * complete the operation that remove 1.5K DP
+		 * pullup and enable 45ohm pulldown on DP/DM.
+		 */
+		delay = ktime_set(0, DWC2_HRTIMER_DIS);
+		break;
+	case PHY_SET_USB_DISC:
+		phy_set_linestate(hsotg->phy, PHY_SET_USB_CONNECT);
+		hsotg->set_linestate = PHY_SET_USB_CONNECT;
+		delay = ktime_set(0, wait_conn_delay);
+		break;
+	case PHY_SET_USB_CONNECT:
+		if (retry--) {
+			phy_set_linestate(hsotg->phy, PHY_SET_USB_DONE);
+			linestate = phy_get_linestate(hsotg->phy);
+			if ((linestate & DP_DM_IS_HIGH)) {
+				phy_set_linestate(hsotg->phy,
+						  PHY_SET_USB_DP_H_DM_L);
+				hsotg->set_linestate = PHY_SET_USB_CONNECT;
+				delay = ktime_set(0, DWC2_HRTIMER_WAIT_SE0);
+				goto restart;
+			} else {
+				dev_dbg(hsotg->dev, "rcv se0 at retry %d delay %ld\n",
+					retry, wait_conn_delay);
+			}
+		} else {
+			dev_dbg(hsotg->dev, "retry timeout at delay %ld\n",
+				wait_conn_delay);
+
+			switch (wait_conn_delay) {
+			case DWC2_HRTIMER_FORCE_DP_H(100):
+				wait_conn_delay = DWC2_HRTIMER_FORCE_DP_H(105);
+				break;
+			case DWC2_HRTIMER_FORCE_DP_H(105):
+				wait_conn_delay = DWC2_HRTIMER_FORCE_DP_H(150);
+				break;
+			case DWC2_HRTIMER_FORCE_DP_H(150):
+				wait_conn_delay = DWC2_HRTIMER_FORCE_DP_H(200);
+				break;
+			case DWC2_HRTIMER_FORCE_DP_H(200):
+				wait_conn_delay = DWC2_HRTIMER_FORCE_DP_H(300);
+				break;
+			case DWC2_HRTIMER_FORCE_DP_H(300):
+				wait_conn_delay = DWC2_HRTIMER_FORCE_DP_H(100);
+				break;
+			default:
+				wait_conn_delay = DWC2_HRTIMER_FORCE_DP_H(100);
+				break;
+			}
+
+			if (wait_conn_delay != DWC2_HRTIMER_FORCE_DP_H(100)) {
+				retry = RETRY_CNT;
+				phy_set_linestate(hsotg->phy,
+						  PHY_SET_USB_DP_H_DM_L);
+				hsotg->set_linestate = PHY_SET_USB_DISC;
+				dev_dbg(hsotg->dev, "retry at delay %ld\n",
+					wait_conn_delay);
+				delay = ktime_set(0, DWC2_HRTIMER_WAIT_SE0);
+				goto restart;
+			}
+		}
+
+		phy_set_linestate(hsotg->phy, PHY_SET_USB_DONE);
+		hsotg->set_linestate = PHY_SET_USB_NONE;
+		delay = ktime_set(0, DWC2_HRTIMER_POLL);
+		break;
+	default:
+		delay = ktime_set(0, DWC2_HRTIMER_POLL);
+		break;
+	}
+
+restart:
+	hrtimer_forward_now(&hsotg->check_linestate_timer, delay);
+	ret = HRTIMER_RESTART;
+	return ret;
+}
+
 /**
  * dwc2_hsotg_dump - dump state of the udc
  * @param: The device state
@@ -4224,6 +4394,11 @@ int dwc2_gadget_init(struct dwc2_hsotg *hsotg, int irq)
 	setup_timer(&hsotg->rst_complete_timer, dwc2_wait_reset_watchdog,
 		    (unsigned long)hsotg);
 
+	hrtimer_init(&hsotg->check_linestate_timer,
+		     CLOCK_MONOTONIC, HRTIMER_MODE_REL);
+	hsotg->check_linestate_timer.function = &dwc2_check_linestate_timer_fn;
+	hsotg->wait_enum_count = 0;
+
 	ret = usb_add_gadget_udc(dev, &hsotg->gadget);
 	if (ret) {
 		dwc2_hsotg_ep_free_request(&hsotg->eps_out[0]->ep,
@@ -4244,6 +4419,8 @@ int dwc2_hsotg_remove(struct dwc2_hsotg *hsotg)
 	usb_del_gadget_udc(&hsotg->gadget);
 	dwc2_hsotg_ep_free_request(&hsotg->eps_out[0]->ep, hsotg->ctrl_req);
 	del_timer_sync(&hsotg->rst_complete_timer);
+	if (usb_linestate_is_enabled())
+		hrtimer_cancel(&hsotg->check_linestate_timer);
 
 	return 0;
 }

drivers/usb/dwc2/hcd.c
@@ -51,9 +51,11 @@
 
 #include <linux/usb/hcd.h>
 #include <linux/usb/ch11.h>
+#include <linux/rockchip/cpu.h>
 
 #include "core.h"
 #include "hcd.h"
+#include "../core/hub.h"
 
 static void dwc2_port_resume(struct dwc2_hsotg *hsotg);
 
@@ -3513,6 +3515,7 @@ static int dwc2_hcd_hub_control(struct dwc2_hsotg *hsotg, u16 typereq,
 				u16 wvalue, u16 windex, char *buf, u16 wlength)
 {
 	struct usb_hub_descriptor *hub_desc;
+	struct usb_hcd *hcd = dwc2_hsotg_to_hcd(hsotg);
 	int retval = 0;
 	u32 hprt0;
 	u32 port_status;
@@ -3750,7 +3753,8 @@ static int dwc2_hcd_hub_control(struct dwc2_hsotg *hsotg, u16 typereq,
 		if (wvalue != USB_PORT_FEAT_TEST && (!windex || windex > 1))
 			goto error;
 
-		if (!hsotg->flags.b.port_connect_status) {
+		if (!hsotg->flags.b.port_connect_status &&
+		    (!HCD_POWER_ON(hcd))) {
 			/*
 			 * The port is disconnected, which means the core is
 			 * either in device mode or it soon will be. Just
@@ -3777,6 +3781,7 @@ static int dwc2_hcd_hub_control(struct dwc2_hsotg *hsotg, u16 typereq,
 			pwr = hprt0 & HPRT0_PWR;
 			hprt0 |= HPRT0_PWR;
 			dwc2_writel(hprt0, hsotg->regs + HPRT0);
+			clear_bit(HCD_FLAG_POWER_ON, &hcd->flags);
 			if (!pwr)
 				dwc2_vbus_supply_init(hsotg);
 			break;
@@ -4433,6 +4438,77 @@ static void dwc2_hcd_reset_func(struct work_struct *work)
 }
 
 /*
+ * High speed reset work queue function
+ */
+static void _dwc2_hcd_hs_reset_work(struct work_struct *work)
+{
+	struct dwc2_hsotg *hsotg = container_of(work, struct dwc2_hsotg,
+						hs_reset_work.work);
+	struct usb_device *rhdev;
+	struct usb_hub *hub;
+	struct usb_hcd *hcd;
+	u32 hprt0, speed;
+	int i;
+
+	if (dwc2_is_device_mode(hsotg))
+		return;
+
+	hcd = dwc2_hsotg_to_hcd(hsotg);
+	if (!hcd) {
+		dev_err(hsotg->dev, "%s: hcd is NULL\n", __func__);
+		return;
+	}
+
+	rhdev = hcd->self.root_hub;
+	hub = usb_hub_to_struct_hub(rhdev);
+
+	for (i = 0; i < rhdev->maxchild; i++) {
+		if ((hub->ports[i]->child) &&
+		    ((hub->ports[i]->child->state) >= USB_STATE_CONFIGURED))
+			return;
+	}
+
+	pr_warn_once("%s start hs handshake\n", __func__);
+	dwc2_disable_host_interrupts(hsotg);
+
+	/* Force both DP and DM low to trigger disconnect */
+	phy_set_linestate(hsotg->phy, PHY_SET_USB_DISC);
+	usleep_range(10000, 11000);
+
+	/* Force DP high and DM low likes high speed connecting */
+	phy_set_linestate(hsotg->phy, PHY_SET_USB_DP_H_DM_L);
+	usleep_range(10000, 11000);
+
+	/* Put port in rest */
+	hprt0 = dwc2_read_hprt0(hsotg);
+	hprt0 &= ~HPRT0_SUSP;
+	hprt0 |= HPRT0_PWR | HPRT0_RST;
+	dwc2_writel(hprt0, hsotg->regs + HPRT0);
+
+	/* Clear reset bit in 50ms (HS) */
+	msleep(50);
+	hprt0 &= ~HPRT0_RST;
+	dwc2_writel(hprt0, hsotg->regs + HPRT0);
+
+	/* Check the device attached or not */
+	hprt0 = dwc2_read_hprt0(hsotg);
+	dev_dbg(hsotg->dev, "after reset, hprt0=%08x\n", hprt0);
+	speed = (hprt0 & HPRT0_SPD_MASK) >> HPRT0_SPD_SHIFT;
+
+	if ((hprt0 & HPRT0_CONNSTS) && (speed == HPRT0_SPD_HIGH_SPEED)) {
+		dev_dbg(hsotg->dev, "hs device is present\n");
+		dwc2_hcd_connect(hsotg);
+	} else {
+		dev_dbg(hsotg->dev, "no device is present\n");
+	}
+
+	/* Reset is done, disable software to set DP and DM voltage */
+	phy_set_linestate(hsotg->phy, PHY_SET_USB_DONE);
+
+	dwc2_enable_host_interrupts(hsotg);
+}
+
+/*
  * =========================================================================
  *  Linux HC Driver Functions
  * =========================================================================
@@ -5024,6 +5100,7 @@ static irqreturn_t _dwc2_hcd_irq(struct usb_hcd *hcd)
 	return dwc2_handle_hcd_intr(hsotg);
 }
 
+#define DWC2_HS_RST_SCHED_DELAY	(5 * HZ)
 /*
  * Creates Status Change bitmap for the root hub and root port. The bitmap is
  * returned in buf. Bit 0 is the status change indicator for the root hub. Bit 1
@@ -5033,6 +5110,30 @@ static irqreturn_t _dwc2_hcd_irq(struct usb_hcd *hcd)
 static int _dwc2_hcd_hub_status_data(struct usb_hcd *hcd, char *buf)
 {
 	struct dwc2_hsotg *hsotg = dwc2_hcd_to_hsotg(hcd);
+	struct usb_device *rhdev;
+	struct usb_hub *hub;
+	int linestate, i;
+	bool sch_work = true;
+
+	if (dwc2_is_host_mode(hsotg) && usb_linestate_is_enabled()) {
+		linestate = phy_get_linestate(hsotg->phy);
+
+		if ((linestate & 0x3) != 0) {
+			rhdev = hcd->self.root_hub;
+			hub = usb_hub_to_struct_hub(rhdev);
+
+			for (i = 0; i < rhdev->maxchild; i++) {
+				if (hub->ports[i]->child &&
+				    (hub->ports[i]->child->state >=
+				     USB_STATE_CONFIGURED))
+					sch_work = false;
+			}
+
+			if (sch_work)
+				schedule_delayed_work(&hsotg->hs_reset_work,
+						      DWC2_HS_RST_SCHED_DELAY);
+		}
+	}
 
 	buf[0] = dwc2_hcd_is_status_changed(hsotg, 1) << 1;
 	return buf[0] != 0;
@@ -5158,6 +5259,8 @@ static void dwc2_hcd_free(struct dwc2_hsotg *hsotg)
 			flush_workqueue(hsotg->wq_otg);
 		destroy_workqueue(hsotg->wq_otg);
 	}
+
+	cancel_delayed_work_sync(&hsotg->hs_reset_work);
 
 	del_timer(&hsotg->wkp_timer);
 }
@@ -5301,6 +5404,9 @@ int dwc2_hcd_init(struct dwc2_hsotg *hsotg, int irq)
 
 	/* Initialize port reset work */
 	INIT_DELAYED_WORK(&hsotg->reset_work, dwc2_hcd_reset_func);
+
+	/* Initialize high speed reset work */
+	INIT_DELAYED_WORK(&hsotg->hs_reset_work, _dwc2_hcd_hs_reset_work);
 
 	/*
 	 * Allocate space for storing data on status transactions. Normally no

drivers/usb/dwc2/core.h
@@ -722,6 +722,9 @@ struct dwc2_hregs_backup {
  *                      - USB_DR_MODE_PERIPHERAL
  *                      - USB_DR_MODE_HOST
  *                      - USB_DR_MODE_OTG
+ * @set_linestate:	set usb dp/dm linestate.
+ * @wait_enum_count	The count used for check_linestate_timer to wait
+ *                      enumeration done.
  * @hcd_enabled		Host mode sub-driver initialization indicator.
  * @gadget_enabled	Peripheral mode sub-driver initialization indicator.
  * @ll_hw_enabled	Status of low-level hardware resources.
@@ -747,6 +750,7 @@ struct dwc2_hregs_backup {
  *                      interrupt
  * @wkp_timer:          Timer object for handling Wakeup Detected interrupt
  * @rst_complete_timer  Timer object for a reset is detected on the USB
+ * @check_linestate_timer Timer used to check usb dp/dm linestate.
  * @lx_state:           Lx state of connected device
  * @gregs_backup: Backup of global registers during suspend
  * @dregs_backup: Backup of device registers during suspend
@@ -823,6 +827,8 @@ struct dwc2_hregs_backup {
  * @status_buf_dma:     DMA address for status_buf
  * @start_work:         Delayed work for handling host A-cable connection
  * @reset_work:         Delayed work for handling a port reset
+ * @hs_reset_work:      Delayed work for handling force DP high and DM low and
+ *                      do a port reset at the same time similar to hs reset.
  * @otg_port:           OTG port number
  * @frame_list:         Frame list
  * @frame_list_dma:     Frame list DMA address
@@ -861,6 +867,8 @@ struct dwc2_hsotg {
 	struct dwc2_core_params *core_params;
 	enum usb_otg_state op_state;
 	enum usb_dr_mode dr_mode;
+	enum usb_linestate set_linestate;
+	u8 wait_enum_count;
 	unsigned int hcd_enabled:1;
 	unsigned int gadget_enabled:1;
 	unsigned int ll_hw_enabled:1;
@@ -888,6 +896,7 @@ struct dwc2_hsotg {
 	struct work_struct wf_otg;
 	struct timer_list wkp_timer;
 	struct timer_list rst_complete_timer;
+	struct hrtimer check_linestate_timer;
 	enum dwc2_lx_state lx_state;
 	struct dwc2_gregs_backup gr_backup;
 	struct dwc2_dregs_backup dr_backup;
@@ -957,6 +966,7 @@ struct dwc2_hsotg {
 
 	struct delayed_work start_work;
 	struct delayed_work reset_work;
+	struct delayed_work hs_reset_work;
 	u8 otg_port;
 	u32 *frame_list;
 	dma_addr_t frame_list_dma;

commit a3c02a3dfbe8fd090fcb106c5a203bf40622643f

usb: dwc2: cancel hrtimer and delayed work when suspend
This patch cancel the hrtimer for dwc2 device mode
and cancel the delayed work for dwc2 host mode which
used for usb linestate controlled when the dwc2 enter
suspend.

Fixes: a5930b4e272d ("usb: dwc2: Fix device and host abnormal usb linestate")
Change-Id: I43a705f7e2a979274e3703944eebce1472204f02
Signed-off-by: William Wu <william.wu@rock-chips.com>

drivers/usb/dwc2/gadget.c
@@ -2834,6 +2834,9 @@ static void dwc2_hsotg_core_disconnect(struct dwc2_hsotg *hsotg)
 {
 	/* set the soft-disconnect bit */
 	__orr32(hsotg->regs + DCTL, DCTL_SFTDISCON);
+
+	if (usb_linestate_is_enabled())
+		hrtimer_cancel(&hsotg->check_linestate_timer);
 }
 
 void dwc2_hsotg_core_connect(struct dwc2_hsotg *hsotg)

drivers/usb/dwc2/hcd.c
@@ -4674,6 +4674,8 @@ skip_power_saving:
 unlock:
 	spin_unlock_irqrestore(&hsotg->lock, flags);
 
+	cancel_delayed_work_sync(&hsotg->hs_reset_work);
+
 	return ret;
 }
 