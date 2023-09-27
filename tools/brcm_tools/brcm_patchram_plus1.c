JeffyCN/mirrors

commit f2550a4649899d37a2ec77abfa17bc19a214b9b2

rkwifibt: update some features
1. support start_wifi/start_bt
2. add rtl8852bt/8821cs/8822cs to wifi-utils.sh
3. add sco over pcm config for brcm

Change-Id: Ib903aebb07fd675adec691af2469e45695606cdc
Signed-off-by: Yao Xiao <xiaoyao@rock-chips.com>
Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

tools/brcm_tools/brcm_patchram_plus1.c
@@ -231,6 +231,10 @@ uchar hci_write_pcm_data_format[] =
 uchar hci_write_i2spcm_interface_param[] =
 	{ 0x01, 0x6d, 0xFC, 0x04, 0x00, 0x00, 0x00, 0x00 };
 
+uchar hci_enable_wbs[]= {
+0x01,0x7e,0xfc,0x03,0x01,0x02,0x00
+}
+
 int
 parse_patchram(char *optarg)
 {
@@ -912,6 +916,11 @@ proc_scopcm()
 void
 proc_i2s()
 {
+	hci_send_cmd(hci_enable_wbs,
+		sizeof(hci_enable_wbs));
+
+	read_event(uart_fd, buffer);
+
 	hci_send_cmd(hci_write_i2spcm_interface_param,
 		sizeof(hci_write_i2spcm_interface_param));
 

scripts/wifibt-init.sh
@@ -7,7 +7,7 @@ wifi_ready()
 
 start_bt_brcm()
 {
-	killall -q brcm_patchram_plus1
+	killall -q -9 brcm_patchram_plus1
 
 	echo 0 > /sys/class/rfkill/rfkill0/state
 	echo 0 > /proc/bluetooth/sleep/btwrite
@@ -25,7 +25,7 @@ start_bt_brcm()
 
 start_bt_rtk_uart()
 {
-	killall -q rtk_hciattach
+	killall -q -9 rtk_hciattach
 
 	echo 0 > /sys/class/rfkill/rfkill0/state
 	echo 0 > /proc/bluetooth/sleep/btwrite
@@ -50,10 +50,13 @@ start_wifi()
 {
 	! wifi_ready || return
 
+	cd "${WIFIBT_MODULE_DIR:-/lib/modules}"
+
 	if [ "$WIFIBT_VENDOR" = Broadcom -a -f dhd_static_buf.ko ]; then
 		insmod dhd_static_buf.ko
 	fi
 
+	echo "Installing WiFi/BT module: $WIFIBT_MODULE"
 	insmod "$WIFIBT_MODULE"
 
 	for i in `seq 100`; do
@@ -68,19 +71,22 @@ start_wifi()
 	echo "Failed to init Wi-Fi for $WIFIBT_CHIP!"
 }
 
-start_wifibt()
+
+start_bt()
 {
 	cd "${WIFIBT_MODULE_DIR:-/lib/modules}"
-
-	start_wifi
 
 	case "$WIFIBT_VENDOR" in
 		Broadcom) start_bt_brcm ;;
 		Realtek)
 			case "$WIFIBT_CHIP" in
-				*E*) start_bt_rtk_usb ;;
+				*[UE]*) start_bt_rtk_usb ;;
 				*) start_bt_rtk_uart ;;
 			esac
+			;;
+		*)
+			echo "Unknow Wi-Fi/BT chip, fallback to Broadcom..."
+			start_bt_brcm
 			;;
 	esac
 }
@@ -94,19 +100,28 @@ fi
 WIFIBT_VENDOR="$(wifibt-util.sh vendor)"
 WIFIBT_MODULE="$(wifibt-util.sh module)"
 
+echo "Handling ${1:-start} for Wi-Fi/BT chip: $(wifibt-util.sh info)"
+
 case "${1:-start}" in
-        start|restart)
+	start|restart)
 		echo "Starting Wifi/BT..."
-		start_wifibt&
+		start_wifi&
+		start_bt&
+		;;
+	start_wifi)
+		start_wifi&
+		;;
+	start_bt)
+		start_bt&
 		;;
 	stop)
-		echo "Stopping Wifi/BT..."
-		killall -q brcm_patchram_plus1 rtk_hciattach || true
-                ;;
-        *)
-                echo "Usage: [start|stop|restart]" >&2
-                exit 3
-                ;;
+		echo "Stopping Wi-Fi/BT..."
+		killall -q -9 brcm_patchram_plus1 rtk_hciattach || true
+		;;
+	*)
+		echo "Usage: [start|stop|start_wifi|start_bt|restart]" >&2
+		exit 3
+		;;
 esac
 
 :

scripts/wifibt-util.sh
@@ -75,8 +75,11 @@ Realtek	RTL8189ES	024c:8179	RTL8189ES.ko
 Realtek	RTL8189FS	024c:f179	RTL8189FS.ko
 Realtek	RTL8192DU	0bda:8194	RTL8192DU.ko
 Realtek	RTL8812AU	0bda:8812	RTL8812AU.ko
+Realtek	RTL8821CS	0bda:c821	RTL8821CS.ko
+Realtek	RTL8822CS	0bda:c822	RTL8822CS.ko
 Realtek	RTL8822BE	10ec:b822	RTL8822BE.ko
 Realtek	RTL8822CE	10ec:c822	RTL8822CE.ko
+Realtek	RTL8852BE	10ec:b852	RTL8852BE.ko
 Broadcom	AP6330	02d0:4330	bcmdhd.ko
 Broadcom	AP6335	02d0:4335	bcmdhd.ko
 Broadcom	AP6354	02d0:4354	bcmdhd.ko

commit df3081dd6a2aa9bcc2837328a6f90955c8a363a9

brcm_tools: Fix a typo
Fixes: f2550a4 ("rkwifibt: update some features")

Change-Id: Ib6f8bfcc9db24dc69ffc097a688c162b5199e901
Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

tools/brcm_tools/brcm_patchram_plus1.c
@@ -231,9 +231,8 @@ uchar hci_write_pcm_data_format[] =
 uchar hci_write_i2spcm_interface_param[] =
 	{ 0x01, 0x6d, 0xFC, 0x04, 0x00, 0x00, 0x00, 0x00 };
 
-uchar hci_enable_wbs[]= {
-0x01,0x7e,0xfc,0x03,0x01,0x02,0x00
-}
+uchar hci_enable_wbs[] =
+	{ 0x01,0x7e,0xfc,0x03,0x01,0x02,0x00 };
 
 int
 parse_patchram(char *optarg)