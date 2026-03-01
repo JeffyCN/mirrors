4580ced6aaca59e6bb836f11e4469c39197b29d6
orangepi <orangepi@local>
2026-03-01 13:07:19 +0800

    rockchipmpp: allow DMABuf sink caps for mppjpegenc

--- gst/rockchipmpp/gstmppjpegenc.c
+++ gst/rockchipmpp/gstmppjpegenc.c
@@ -75,7 +75,11 @@ static GstStaticPadTemplate gst_mpp_jpeg_enc_sink_template =
 GST_STATIC_PAD_TEMPLATE ("sink",
     GST_PAD_SINK,
     GST_PAD_ALWAYS,
-    GST_STATIC_CAPS ("video/x-raw,"
+    GST_STATIC_CAPS (
+        "video/x-raw,"
+        "format = (string) { " MPP_ENC_FORMATS " }, "
+        GST_MPP_JPEG_ENC_SIZE_CAPS ";"
+        "video/x-raw(memory:DMABuf),"
         "format = (string) { " MPP_ENC_FORMATS " }, "
         GST_MPP_JPEG_ENC_SIZE_CAPS));
 
